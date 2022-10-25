7d9a27e80f2ccc9dd75327d55c0459b2c1ab8021
Gonzalo Pacheco <gonzalopachecozamorano@gmail.com>
2022-10-25 00:31:55 -0300

    Primer commit de mi heroes app

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom"
+import { LoginPage } from "../auth/"
+import { Navbar } from '../ui'
+import { HeroesRoutes } from "../heroes"
+
+
+export const AppRouter = () => {
+
+  return (
+    <>
+
+        <Routes>
+            <Route path="login" element={<LoginPage />} />
+            <Route path="/*" element={<HeroesRoutes />} />
+        </Routes>
+    </>
+  )
+
+}
---
+++ src/ui/components/Navbar.jsx
@@ -0,0 +1,81 @@
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+
+
+export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        })
+    }
+
+
+
+    return (
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
+            
+            <Link 
+                className="navbar-brand" 
+                to="/"
+            >
+                Asociaciones
+            </Link>
+
+            <div className="navbar-collapse">
+                <div className="navbar-nav">
+
+                    <NavLink 
+                        className={ ({isActive}) => `nav-link ${isActive ? 'active' : ''}` } 
+                        to="/marvel"
+                        end
+                    >
+                        Marvel
+                    </NavLink>
+
+                    <NavLink 
+                        className={ ({isActive}) => `nav-link ${isActive ? 'active' : ''}` }
+                        to="/dc"
+                        end
+                    >
+                        DC
+                    </NavLink>
+
+                    <NavLink 
+                        className={ ({isActive}) => `nav-link ${isActive ? 'active' : ''}` }
+                        to="/hero"
+                        end
+                    >
+                        Hero
+                    </NavLink>
+
+                    <NavLink 
+                        className={ ({isActive}) => `nav-link ${isActive ? 'active' : ''}` }
+                        to="/search"
+                        end
+                    >
+                        Search
+                    </NavLink>
+                </div>
+            </div>
+
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
+                <ul className="navbar-nav ml-auto">
+                    
+                    <span className='nav-item nav-link text-primary'>
+                        Gonzalo
+                    </span>
+
+                    <button 
+                        className='nav-item nav-link btn'
+                        onClick={onLogout}
+                    >
+                        Logout
+                    </button>
+
+                </ul>
+            </div>
+        </nav>
+    )
+}
